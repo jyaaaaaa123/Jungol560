d032718f17b440a2c6d29e76366c70f9a55f71ff
kangJinYoung123 <72846139+kangJinYoung123@users.noreply.github.com>
2020-11-05 09:47:35 +0900

    2nd commit

--- Main5.java
+++ Main5.java
@@ -6,11 +6,15 @@ public class Main5 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int[] ar = new int[10];
+		
 		for (int i = 0; i < ar.length; i++) {
 			ar[i] = sc.nextInt();
 		}
+		
 		Arrays.sort(ar);
 		System.out.println(ar[0]);
 		sc.close();
+		
+		
 	}
 }
